9a615ab2f896a1b1f186bc2301388f6eec4d4bb6
kevin kiplangat <kiplangatkevin335@gmail.com>
2025-10-31 11:27:27 +0300

    fav working

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -4,14 +4,16 @@ import useLocalStorage from "../hooks/useLocalStorage";
 
 function Favorites() {
   const [likedBooks, setLikedBooks] = useLocalStorage("likedBooks", []);
-
-  return (
+return (
     <div className="p-4">
+        
       <h1 className="text-2xl font-bold mb-4 text-center">Your Favorite Books</h1>
-
       {likedBooks.length > 0 ? (
+
+
         <BookList
           books={likedBooks}
+          
           likedBooks={likedBooks}
           toggleLike={(book) =>
             setLikedBooks((prev) =>
@@ -21,7 +23,8 @@ function Favorites() {
           libraryBooks={[]} 
           toggleLibrary={() => {}}
         />
-      ) : (
+      ) : 
+      (
         <p className="text-center text-gray-600 dark:text-gray-400">
           You haven’t liked any books yet. Browse and tap the heart icon to add some!
         </p>
